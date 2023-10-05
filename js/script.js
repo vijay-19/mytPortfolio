ac3b0b00898466f5f6f5aa0e6e88dd3dfdac2d22
Vijaya Bharathi Vinayagam <vijayabharathi.vinayagam@rrd.com>
2023-10-05 23:49:44 +0530

    Added headers and bg circles code commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+// Toggle Navbar
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+// Active Section
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if (e.target.classList.contains("nav-item")) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0, 0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+
+        }, 500);
+    }
+
+});
+
+
 // About tabs
 
 const tabsContainer = document.querySelector(".about-tabs");
