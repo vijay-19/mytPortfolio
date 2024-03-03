ddbb79d5e6da96a67c6e576343477e9b6667f421
Vijaya Bharathi Vinayagam <vijayabharathi.vinayagam@rrd.com>
2024-03-03 13:53:54 +0530

    Added my portfolio page and added styles and scripts code

--- js/script.js
+++ js/script.js
@@ -55,3 +55,39 @@ tabsContainer.addEventListener("click", (e) => {
         aboutSection.querySelector(target).classList.add("active");
     }
 });
+
+
+// Portfolio Items
+
+document.addEventListener("click", (e) => {
+if(e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0,0);
+    portfolioItemDetails(e.target.parentElement);
+}
+});
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+// Hide popup  when clicking outside of it
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("pp-inner")) {
+        togglePortfolioPopup();
+    }
+})
+
+function portfolioItemDetails() {
+    document.querySelector(".pp-thumbnail img").src = 
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
